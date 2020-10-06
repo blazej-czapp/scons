c8976a6bbc344d0cd9ac5bc7a5d6b1f0c53b2c26
Blazej Czapp <master@constant.space>
2020-10-06 10:53:29 +0100

    Make a fake release?

--- SCons/__init__.py
+++ SCons/__init__.py
@@ -1,9 +1,9 @@
-__version__="4.0.1"
+__version__="4.0.2bc"
 __copyright__="Copyright (c) 2001 - 2020 The SCons Foundation"
 __developer__="bdbaddog"
 __date__="2020-07-17 01:50:03"
 __buildsys__="ProDog2020"
-__revision__="c289977f8b34786ab6c334311e232886da7e8df1"
-__build__="c289977f8b34786ab6c334311e232886da7e8df1"
+__revision__="c289977f8b34786ab6c334311e232886da7e8df2"
+__build__="c289977f8b34786ab6c334311e232886da7e8df2"
 # make sure compatibility is always in place
 import SCons.compat # noqa
